51643e891dceb9ca3ccc1137b167f48e19143133
Kubicek <82667968+kubicek-skoleni@users.noreply.github.com>
2026-01-26 12:58:35 +0100

    linq ukoly  2 - anon typ, tuple, trida, select

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,4 +1,7 @@
-﻿int[] numbers = { 11, 2, 13, -97542, 44, -5, 6, 127, -99, 0, 256, 0, 12, 11 };
+﻿using ConsoleApp;
+using System;
+
+int[] numbers = { 11, 2, 13, -97542, 44, -5, 6, 127, -99, 0, 256, 0, 12, 11 };
 
 //// 1.  kolik obsahuje pole kladných čísel
 //// 2.  kolik obsahuje pole záporných čísel
@@ -37,7 +40,46 @@ Console.WriteLine($"skip3sum: {skip3sum}");
 var fruits = new[] { "aPPLE", "BlUeBeRrY", "cHeRry", "RaspbeRry" };
 
 //// zjistěte kolik obsahují všechna slova v poli "fruits" dohromady 
+var dohromady_pismen = fruits.Select(x => x.Length).Sum();
+Console.WriteLine($"dohromady pismen {dohromady_pismen}");
 
-////lowercase
+////
+var lowercaseall = fruits.Select(x => x.ToLower());
+Console.WriteLine($"lowercaseall: {string.Join(", ", lowercaseall)}");
 
 //// uppercase + lowercase
+var upper_lower_class = fruits.Select(x => new WordUpperLower(x));
+
+// tuple - seskupení hodnot
+var upper_lower_anon = fruits.Select(x => new 
+{
+    UpperCase = x.ToUpper(),
+    LowerCase = x.ToLower()
+});
+
+var upper_lower_tupple = fruits.Select(x => (Lower: x.ToLower(), Upper: x.ToUpper()));
+
+foreach (var item in upper_lower_tupple)
+{
+    Console.WriteLine($"Upper: {item.Upper}, Lower: {item.Lower}");
+}
+
+
+int.TryParse("123", out int result);
+
+(bool success, int value) ModernParse(string input)
+{
+    bool result = int.TryParse(input, out int value);
+    return (success: result, value: value);
+}
+
+var parseresult = ModernParse("333333");
+
+if(parseresult.success)
+{
+    Console.WriteLine($"Parsed value: {parseresult.value}");
+}
+else
+{
+    Console.WriteLine("Parsing failed.");
+}
---
+++ ConsoleApp/WordUpperLower.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp
+{
+    internal class WordUpperLower
+    {
+        public WordUpperLower(string word)
+        {
+            UpperCase = word.ToUpper();
+            LowerCase = word.ToLower();
+        }
+        public string UpperCase { get; set; }
+        public string LowerCase { get; set; }
+    }
+}

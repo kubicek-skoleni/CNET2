ee3ca412346fd6f7fa4e32cd901b3c93a844e23b
Kubicek <82667968+kubicek-skoleni@users.noreply.github.com>
2026-01-26 13:47:22 +0100

    student + getter / setter

---
+++ ConsoleApp/Model/Student.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp.Model
+{
+    public class Student
+    {
+        // Jmeno, Prijmeni, RokNarozeni, Trida, Adresa
+
+        private string prijmeni;
+
+        public string Prijmeni {
+            get 
+            {
+                return prijmeni;
+            }
+            set
+            {
+                value = value.Trim();
+                var prvni = new string(value.Take(1).ToArray()).ToUpper();
+                var zbytek = value.Substring(1).ToLower();
+                prijmeni = prvni + zbytek;
+            }
+
+        }
+        public int RokNarozeni { get; set; }
+        public string Trida { get; set; }
+        public string Adresa { get; set; }
+    }
+}
--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,7 +1,12 @@
-﻿var numbers = new[] { 5, 4, 1, 3, 9, 8, 6, 7, 2, 0 };
-var strings = new[] { "zero", "one", "two", "three", "four", "five"
-    , "six", "seven", "eight", "nine" };
-// vypište čísla v poli numbers jako slova
+﻿
 
-var jako_slova = numbers.Select(num => strings[num]);
-Console.WriteLine($"jako slova: {string.Join(", ", jako_slova)}");
+using ConsoleApp.Model;
+
+var student = new Student();
+
+student.Prijmeni = "  novak  ";
+student.RokNarozeni = 2005;
+student.Trida = "9.A";
+student.Adresa = "Hlavni 123, Praha";
+
+Console.WriteLine(student.Prijmeni);

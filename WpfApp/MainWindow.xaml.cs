36b7b6be7cdd5657739886ec5fe0046657f76ef6
Kubicek <82667968+kubicek-skoleni@users.noreply.github.com>
2026-01-28 10:52:47 +0100

    random bg brush button

--- WpfApp/MainWindow.xaml.cs
+++ WpfApp/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.IO;
+﻿using System.Diagnostics;
+using System.IO;
+using System.Reflection;
 using System.Text;
 using System.Windows;
 using System.Windows.Controls;
@@ -24,6 +26,9 @@ namespace WpfApp
 
         private void btnSeq1_Click(object sender, RoutedEventArgs e)
         {
+            Stopwatch time = new Stopwatch();
+            time.Start();
+
             txbInfo.Text = "";
             string filesdir = @"C:\Users\Student\Downloads\bigfiles";
 
@@ -54,8 +59,70 @@ namespace WpfApp
                     txbInfo.Text += $"   {item.Key} - {item.Value}{Environment.NewLine}";
                 }
                 txbInfo.Text += Environment.NewLine;
-
             }
+
+            time.Stop();
+            txbInfo.Text += $"Čas zpracování: {time.ElapsedMilliseconds} ms";
+        }
+
+        private void btnSeq2_Click(object sender, RoutedEventArgs e)
+        {
+            Mouse.OverrideCursor = System.Windows.Input.Cursors.Wait;
+
+            Stopwatch time = new Stopwatch();
+            time.Start();
+
+            txbInfo.Text = "";
+            string filesdir = @"C:\Users\Student\Downloads\bigfiles";
+
+            List<string> files = Directory.EnumerateFiles(filesdir, "*.txt")
+                                          .ToList();
+
+            Dictionary<string, int> wordCount = new();
+
+            foreach (var file in files)
+            {
+                var words = File.ReadAllLines(file);
+
+                foreach (var word in words)
+                {
+                    if (wordCount.ContainsKey(word))
+                        wordCount[word]++;
+                    else
+                        wordCount.Add(word, 1);
+                }
+            }
+
+            var top10 = wordCount
+                            .OrderByDescending(x => x.Value)
+                            .Take(10);
+
+            foreach (var item in top10)
+            {
+                txbInfo.Text += $"{item.Key} - {item.Value}{Environment.NewLine}";
+            }
+            txbInfo.Text += Environment.NewLine;
+
+            time.Stop();
+            txbInfo.Text += $"Čas zpracování: {time.ElapsedMilliseconds} ms";
+
+            Mouse.OverrideCursor = null;
+        }
+
+        private void btnColor_Click(object sender, RoutedEventArgs e)
+        {
+            btnColor.Background = GetRandomSolidBrush();
+        }
+
+        Brush GetRandomSolidBrush()
+        {
+            var brushes = typeof(Brushes)
+                .GetProperties(BindingFlags.Public | BindingFlags.Static)
+                .Select(p => p.GetValue(null))
+                .OfType<SolidColorBrush>()
+                .ToList();
+
+            return brushes[Random.Shared.Next(brushes.Count)];
         }
     }
 }

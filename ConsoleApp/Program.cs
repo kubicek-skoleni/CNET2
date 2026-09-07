04409e5f3ca3f5d3aba5831459fdc8892ae12689
kubicek-skoleni <82667968+kubicek-skoleni@users.noreply.github.com>
2026-09-07 10:19:52 +0200

    orgranizace v program.cs - main

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,1 +1,6 @@
 ﻿Console.WriteLine("Hello, World 2!");
+
+void SayHello(string name)
+{
+    Console.WriteLine($"Hello, {name}!");
+}   
---
+++ ConsoleApp/Program2.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp
+{
+    internal class Program2
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello from Program2!");
+
+            void SayHello(string name)
+            {
+                Console.WriteLine($"Hello, {name}!");
+            }
+        }
+    }
+}

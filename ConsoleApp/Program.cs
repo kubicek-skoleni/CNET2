86049395ef5fabc1ad881ee0df0770c3ef538147
kubicek-skoleni <82667968+kubicek-skoleni@users.noreply.github.com>
2026-09-07 13:02:27 +0200

    metody reseni

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,12 +1,29 @@
 ﻿
 // 1. udělejte metodu která vrátí větši ze dvou celých čísel
 
+// Greater, Vetsi, Vetsi, VetsiCislo
+// Max
+
+int Vetsi(int a, int b, int c) 
+            => (a > b) ? a : b;
 
 // 2. metodu FullName, která spojí
 // jméno a příjmení do jednoho řetězce a vrátí ho
 
+string FullName(string firstName, string lastName) 
+    => $"{firstName}    {lastName}";
+
 
 // 3. metodu na kontrolu validity hesla (delší než 8 znaků)
+// PassworCheck, IsValid, PlatneHeslo, JePlatneHeslo
+
+bool IsValid(string password) => password.Length > 8;
 
 
 // 4. metodu na převod minut na sekundy
+// PrevedMinNaSek, PocetSekund, MinutesToSeconds
+
+int MinutesToSeconds(int minutes)
+{
+    return minutes * 60;
+}

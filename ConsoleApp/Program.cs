436fe4221cda77f0e79389112d3da862b8665025
kubicek-skoleni <82667968+kubicek-skoleni@users.noreply.github.com>
2026-09-07 13:46:53 +0200

    logovani chyby do souboru

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -5,13 +5,19 @@ try
     numbers[5] = 100;
     Console.WriteLine(numbers[5]);
 }
-catch(IndexOutOfRangeException)
-{
-    Console.WriteLine($"Chyba: Index mimo rozsah");
-}
+//catch(IndexOutOfRangeException)
+//{
+//    Console.WriteLine($"Chyba: Index mimo rozsah");
+//}
 catch(Exception ex)
 {
-    Console.WriteLine($"Chyba: {ex.Message}");
+    DateTime currentTime = DateTime.Now;
+
+    var fileName = currentTime.ToString("yyyy-MM-dd_HH-mm-ss") + "_error_log.txt";
+    var message = $"{ex.Message}{Environment.NewLine}{ex.StackTrace}";
+
+    File.WriteAllText(fileName, message);
+    Console.WriteLine($"Chyba zaznamenana do souboru: {fileName}");
 }
 
 Console.WriteLine("pokračuji");

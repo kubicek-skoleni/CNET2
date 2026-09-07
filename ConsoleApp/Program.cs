c4fe9043b5d53ccaba41f9811113617cc513900a
kubicek-skoleni <82667968+kubicek-skoleni@users.noreply.github.com>
2026-09-07 12:23:59 +0200

    metody

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,8 +1,16 @@
 ﻿
-double FromCelsiusToFahrenheit(double celsius, int precision)
+var prec = 10 + 2;
+
+FromCelsiusToFahrenheit(celsius: 10.5, precision: prec);
+
+double FromCelsiusToFahrenheit(double celsius, int precision = 2)
 {
     double fahrenheit = (celsius * 9 / 5) + 32;
     fahrenheit = Math.Round(fahrenheit, precision);
-
     return fahrenheit;
+}
+
+void PrintHello(string name)
+{
+    Console.WriteLine($"Hello, {name}!");
 }

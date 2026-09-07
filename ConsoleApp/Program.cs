9900384f9c280fc76e38e078667e5b2e122e0eb6
kubicek-skoleni <82667968+kubicek-skoleni@users.noreply.github.com>
2026-09-07 13:28:55 +0200

    tuple

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,10 +1,22 @@
 ﻿
-// 1. udělejte metodu která vrátí větši ze dvou celých čísel
+Console.WriteLine($"Vetsi(10, 20) = {Vetsi(10, 20)}");
+Console.WriteLine($"Fullname(Pavel, Kubát) {FullName("Pavel", "Kubát")}");
+Console.WriteLine($"IsValid(password123) {IsValid("password123")}");
+Console.WriteLine($"MinutesToSeconds(3) {MinutesToSeconds(3)}");
 
+//tuple - přes tečky
+var result = SecondsToMinutes(125);
+Console.WriteLine($"SecondsToMinutes(125) = {result.minutes} min, {result.over} sec");
+
+//tuple - přes deconstruction
+(int min, int sec) = SecondsToMinutes(190);
+Console.WriteLine($"SecondsToMinutes(190) = {min} min, {sec} sec");
+
+// 1. udělejte metodu která vrátí větši ze dvou celých čísel
 // Greater, Vetsi, Vetsi, VetsiCislo
 // Max
 
-int Vetsi(int a, int b, int c) 
+int Vetsi(int a, int b) 
             => (a > b) ? a : b;
 
 // 2. metodu FullName, která spojí
@@ -27,3 +39,11 @@ int MinutesToSeconds(int minutes)
 {
     return minutes * 60;
 }
+
+(int minutes, int over) SecondsToMinutes(int seconds)
+{
+    int minutes = seconds / 60;
+    int over = seconds % 60;
+    return (minutes, over);
+}
+

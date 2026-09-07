4a2987903db678fc2540aa07f19d1408f3b6e76f
kubicek-skoleni <82667968+kubicek-skoleni@users.noreply.github.com>
2026-09-07 12:31:56 +0200

    zadani metod

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,16 +1,12 @@
 ﻿
-var prec = 10 + 2;
+// 1. udělejte metodu která vrátí větši ze dvou celých čísel
 
-FromCelsiusToFahrenheit(celsius: 10.5, precision: prec);
 
-double FromCelsiusToFahrenheit(double celsius, int precision = 2)
-{
-    double fahrenheit = (celsius * 9 / 5) + 32;
-    fahrenheit = Math.Round(fahrenheit, precision);
-    return fahrenheit;
-}
+// 2. metodu FullName, která spojí
+// jméno a příjmení do jednoho řetězce a vrátí ho
 
-void PrintHello(string name)
-{
-    Console.WriteLine($"Hello, {name}!");
-}
+
+// 3. metodu na kontrolu validity hesla (delší než 8 znaků)
+
+
+// 4. metodu na převod minut na sekundy

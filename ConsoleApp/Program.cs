e506d8590d48c4121c993fb4568198545cc6040d
kubicek-skoleni <82667968+kubicek-skoleni@users.noreply.github.com>
2026-09-08 13:41:20 +0200

    capitalize first letter linq

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,28 +1,34 @@
 ﻿
+using ConsoleApp;
+
 var fruits = new[] { "aPPLE", "BlUeBeRrY", 
-    "cHeRry", "RaspbeRry" };
+    "cHeRry", "RaspbeRry", "čučoriedka" };
 
 
 // vypište všechno jako lowercase
 
-var lowercaseFruits = fruits
-                .Select(ovoce => ovoce.ToLower());
-Console.WriteLine($"Lowercase fruits: {string.Join(", ", lowercaseFruits)}");
+//var lowercaseFruits = fruits
+//                .Select(ovoce => ovoce.ToLower());
+//Console.WriteLine($"Lowercase fruits: {string.Join(", ", lowercaseFruits)}");
 
 // vypište velkými písmeny,
 // seřazeno od nejdelšího po nejkratší
 
-var result = fruits.Select(f => f.ToUpper())
-    .OrderByDescending(f => f.Length);
+//var result = fruits.Select(f => f.ToUpper())
+//    .OrderByDescending(f => f.Length);
 
-Console.WriteLine($"Uppercase fruits ordered by length: {string.Join(", ", result)}");
+//Console.WriteLine($"Uppercase fruits ordered by length: {string.Join(", ", result)}");
 
 
-// kapitalizace prního písmene
-string CapitalizeFirstLetter(string input)
-{
-    if (string.IsNullOrEmpty(input))
-        return input;
+// **********
 
-    return char.ToUpper(input[0]) + input.Substring(1).ToLower();
-}
+
+
+//  vypište s prvními velkými písmeny
+var capFruits = fruits
+    .Select(slovo =>
+    TextTransform.CapitalizeFirstLetter(slovo));
+
+Console.WriteLine($"Capitalized fruits: {string.Join(", ", capFruits)}");
+
+
---
+++ ConsoleApp/TextTransform.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp;
+
+public class TextTransform
+{
+    // kapitalizace prvního písmene
+    public static string CapitalizeFirstLetter(string input)
+    {
+        if (string.IsNullOrEmpty(input))
+            return input;
+
+        var firstLetter = input[0];
+        return char.ToUpper(firstLetter)
+            + input.Substring(1).ToLower();
+    }
+}

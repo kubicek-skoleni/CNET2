bf9da2c57f1bf85f9fbb149b67f73fb3a331a4e4
kubicek-skoleni <82667968+kubicek-skoleni@users.noreply.github.com>
2026-09-08 13:13:44 +0200

    pole fruits

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,42 +1,5 @@
-﻿using ConsoleApp.Model;
+﻿
 
-int[] numbers = { 11, 2, 13, -97542, 44, -5, 6, 127, -99, 0, 256, 0, 12, 11 };
-
-//// 1.  kolik obsahuje pole kladných čísel
-//// 2.  kolik obsahuje pole záporných čísel
-//// 3.  sumu kladných hodnot
-//// 4.  největší absolutní hodnotu
-//// 5.  všechna kladná sudá čísla
-//// 6.  seřaďte pole od nejmenších po největší hodnoty,
-//// 7.  přeskočte první 3 prvky a sečtěte zbytek hodnot
-///
+var fruits = new[] { "aPPLE", "BlUeBeRrY", "cHeRry", "RaspbeRry" };
 
 
-var pocet_kladnych = numbers.Where(x => x > 0).Count();
-Console.WriteLine($"pocet_kladnych: {pocet_kladnych}");
-
-var pocet_zapornych = numbers.Where(x => x < 0).Count();
-Console.WriteLine($"pocet_zapornych: {pocet_zapornych}");
-
-var suma_kladnych = numbers.Where(x => x > 0).Sum();
-Console.WriteLine($"suma_kladnych: {suma_kladnych}");
-
-var nejv_abs = numbers.Select(x => Math.Abs(x)).Max();
-Console.WriteLine($"nejv_abs: {nejv_abs}");
-
-
-var kladna_suda = numbers.Where(cislo => cislo > 0 && cislo % 2 == 0);
-var kladna_suda_string = string.Join(", ", kladna_suda);
-Console.WriteLine(kladna_suda_string);
-
-var ordered = numbers.OrderBy(x => x);
-Console.WriteLine($"ordered: {string.Join(", ", ordered)}");
-
-var skip3sum = numbers.Skip(3).Sum();
-Console.WriteLine($"skip3sum: {skip3sum}");
-
-// jsou všechna čisla v poli kladná?
-// je alespoň jedno číslo v poli kladné?
-
-bool vsechna_kladna = numbers.All(x => x > 0);
-bool alespon_jedno_kladne = numbers.Any(x => x > 0);

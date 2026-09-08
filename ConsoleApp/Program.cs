6e1cd16d899b39530eb127a7aad00a6208806e8c
kubicek-skoleni <82667968+kubicek-skoleni@users.noreply.github.com>
2026-09-08 14:14:01 +0200

    Zadani pro Classes - adresa, bank ucet, kostka

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,13 +1,6 @@
-﻿var numbers = new[] { 5, 4, 1, 3, 9, 8, 6, 7, 2, 0 };
+﻿// třidy pro
 
-var strings = new[] { "zero", "one", "two",
-    "three", "four", "five", "six", "seven", 
-    "eight", "nine" };
-
-
-// vypište čísla v poli numbers jako slova
-
-var jako_slova = numbers.Select(n => strings[n]);
-
-Console.WriteLine($"jako slova: {string.Join(", ", jako_slova)}");
-
+// Adresu - Ulice, Mesto, PSC, Stat
+// Bankovni ucet - Cislo uctu, Majitel, Zustatek
+//              - operace - Vklad, Vyber
+// Kostka - pocet stran + hod kostkou

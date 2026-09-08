fb354b93648163a48635434f739b805598d915f4
kubicek-skoleni <82667968+kubicek-skoleni@users.noreply.github.com>
2026-09-08 14:53:59 +0200

    private set

--- ConsoleApp/Model/BankovniUcet.cs
+++ ConsoleApp/Model/BankovniUcet.cs
@@ -6,19 +6,18 @@ namespace ConsoleApp.Model;
 
 public class BankovniUcet
 {
-    private decimal _zustatek;
     public string CisloUctu { get; set; } = string.Empty;
 
     public string Majitel { get; set; } = string.Empty;
 
-    public decimal Zustatek { get { return _zustatek; } }
+    public decimal Zustatek { get; private set; }
 
     public decimal Vlozit(decimal castka)
     {
         if (castka <= 0)
             throw new ArgumentException("Částka musí být kladná.", nameof(castka));
-        
-        _zustatek += castka;
+
+        Zustatek += castka;
         
         return Zustatek;
     }
@@ -31,7 +30,7 @@ public class BankovniUcet
         if (castka > Zustatek)
             throw new InvalidOperationException("Nedostatečný zůstatek na účtu.");
 
-        _zustatek -= castka;
+        Zustatek -= castka;
 
         return Zustatek;
     }

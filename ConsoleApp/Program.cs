03625f27bd0231311e20e72279bbd8b1e1b6dbab
kubicek-skoleni <82667968+kubicek-skoleni@users.noreply.github.com>
2026-09-08 13:27:16 +0200

    metoda CapitalizeFirstLetter

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,8 +1,28 @@
 ﻿
-
 var fruits = new[] { "aPPLE", "BlUeBeRrY", 
     "cHeRry", "RaspbeRry" };
 
 
+// vypište všechno jako lowercase
+
+var lowercaseFruits = fruits
+                .Select(ovoce => ovoce.ToLower());
+Console.WriteLine($"Lowercase fruits: {string.Join(", ", lowercaseFruits)}");
+
+// vypište velkými písmeny,
+// seřazeno od nejdelšího po nejkratší
+
+var result = fruits.Select(f => f.ToUpper())
+    .OrderByDescending(f => f.Length);
+
+Console.WriteLine($"Uppercase fruits ordered by length: {string.Join(", ", result)}");
 
 
+// kapitalizace prního písmene
+string CapitalizeFirstLetter(string input)
+{
+    if (string.IsNullOrEmpty(input))
+        return input;
+
+    return char.ToUpper(input[0]) + input.Substring(1).ToLower();
+}

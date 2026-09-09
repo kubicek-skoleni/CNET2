8605f5acd74f8e254ebf522c723d7d6d28482f26
kubicek-skoleni <82667968+kubicek-skoleni@users.noreply.github.com>
2026-09-09 13:33:21 +0200

    db context tables

---
+++ Corp.Database/PeopleContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Corp.Model;
+
+namespace Corp.Database;
+
+public class PeopleContext : DbContext
+{
+    public DbSet<Person> Persons { get; set; }
+
+    public DbSet<Address> Addresses { get; set; }
+
+    public DbSet<Contract> Contracts { get; set; }
+
+
+}

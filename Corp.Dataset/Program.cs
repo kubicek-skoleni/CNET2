300e177598f08224db3ce347db4506a071e46b2c
kubicek-skoleni <82667968+kubicek-skoleni@users.noreply.github.com>
2026-09-09 10:24:12 +0200

    nacteni datasetu

---
+++ Corp.Dataset/Program.cs
@@ -0,0 +1,14 @@
+﻿using Corp.Model;
+using System.Text.Json;
+
+Console.WriteLine("Hello, World!");
+
+string filePath = @"C:\Users\Student\source\repos\kubicek3\CNET2\data2024.json";
+var content = File.ReadAllText(filePath);
+
+var people 
+    = JsonSerializer.Deserialize<List<Person>>(content);
+
+Console.WriteLine($"Počet osob: {people.Count}");
+
+

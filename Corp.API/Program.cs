c752b14d6176a99a77ddcfb369205057c5de8986
kubicek-skoleni <82667968+kubicek-skoleni@users.noreply.github.com>
2026-09-09 12:48:01 +0200

    dummy person by id

--- Corp.API/Program.cs
+++ Corp.API/Program.cs
@@ -1,3 +1,5 @@
+using Corp.Model;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -20,6 +22,8 @@ app.MapGet("/person/{id:int}", (int id) =>
         FirstName = "Nepojmenovaný",
         LastName = "Nepříjmenovaný"
     };
+
+    return person;
 });
 
     

c82bba5ea9cc681dfec05190a00cdc2f2c76c41a
kubicek-skoleni <82667968+kubicek-skoleni@users.noreply.github.com>
2026-09-09 09:53:02 +0200

    Corp.Model tridy

---
+++ Corp.Model/Address.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Corp.Model;
+
+public class Address
+{
+    public int Id { get; set; }
+    public string Street { get; set; }
+    public string City { get; set; }
+    public string ZipCode { get; set; }
+}
---
+++ Corp.Model/Person.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.Text;
+
+namespace Corp.Model;
+
+public class Person
+{
+    public int Id { get; set; }
+
+    public string FirstName { get; set; }
+
+    public string LastName { get; set; }
+
+    public string Email { get; set; }
+
+    public int ChildrenCount { get; set; }
+
+    public DateTime DateOfBirth { get; set; }
+
+    public Address? Address { get; set; }
+
+    public List<Contract> Contracts { get; set; }
+}

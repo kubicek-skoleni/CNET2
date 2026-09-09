8d185599439c56f46848678a0d7a1cc0f71597f2
kubicek-skoleni <82667968+kubicek-skoleni@users.noreply.github.com>
2026-09-09 09:05:07 +0200

    kostka

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -19,7 +19,14 @@ Console.WriteLine("*********");
 
 var kostka6 = new Kostka(6);
 
+List<int> hody = new List<int>();
+
 for (int i = 0; i < 20; i++)
 {
-    Console.WriteLine($"Hod kostkou: {kostka6.Hod()}");
+    var hod = kostka6.Hod();
+    hody.Add(hod);
+    Console.WriteLine($"Hod kostkou: {hod}");
 }
+
+var prumer = hody.Average();
+var pocet_hodu = hody.Count();
